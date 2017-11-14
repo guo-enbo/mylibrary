7f3d74f32cb93adc37829dec2855803d91ea7e25
geb1024 <25416133399@qq.com>
2017-11-14 15:53:09 +0800

    初次导入

---
+++ app/src/main/java/com/qiji/mylibrary/widget/flowlayout/OnInitSelectedPosition.java
@@ -0,0 +1,9 @@
+package com.qiji.fingertipfinancial.widget.flowlayout;
+
+/**
+ * Created by guoenbo on 17/09/20.
+ */
+
+public interface OnInitSelectedPosition {
+    boolean isSelectedPosition(int position);
+}
---
+++ app/src/main/java/com/qiji/mylibrary/widget/flowlayout/OnTagClickListener.java
@@ -0,0 +1,10 @@
+package com.qiji.fingertipfinancial.widget.flowlayout;
+
+import android.view.View;
+
+/**
+ * Created by guoenbo on 17/09/20.
+ */
+public interface OnTagClickListener {
+    void onItemClick(FlowTagLayout parent, View view, int position);
+}
